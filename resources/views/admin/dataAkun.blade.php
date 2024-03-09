f6865ec5b69d7131bcecf752a9881fa35cfbd405
zoin <jointaras@gmail.com>
2024-03-09 11:02:22 +0700

    kelola akun

--- resources/views/admin/dataAkun.blade.php
+++ resources/views/admin/dataAkun.blade.php
@@ -1,17 +1,105 @@
 @extends('layouts.app')
 @section('content')
     <div class="content-wrapper">
-        <div class="card" style="background:#1265A8" padding:10px;">
-            <div class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                    </div>
-                </div>
+
+        <div class="container-fluid">
+            <div class="card" style="background:#1265A8">
                 <!-- /.content-header -->
 
+                <div class="row p-5">
+                    <div class="col-lg-4 col-sm-12">
+                        <form action="/kendaraan/search" class="form-inline" method="GET">
+                            <div class="input-group " style="flex-grow: 10;">
+                                <input type="search" class="form-control mr-10" style="width: 200px;" name="search"
+                                    id="cariDataKendaraan" placeholder="Cari Akun Pengguna...">
+                                <div class="input-group-append">
+                                    <button class="btn btn-primary ml-1" type="submit"><i class="iconify"
+                                            data-icon="material-symbols:search"></i> Cari</button>
+                                    <a href="/tipeKendaraanUser" class="btn btn-danger ml-1"><i class="iconify"
+                                            data-icon="solar:refresh-linear"></i> Reset</a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+
+                    <div class="col-lg-8 col-sm-12">
+                        <div class="float-right">
+                            <!-- Modal -->
+                            <div class="modal fade" id="modalLoginForm" tabindex="-1" role="dialog"
+                                aria-labelledby="myModalLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content" style="color:white; background:#1265A8; padding:10px;">
+                                        <div class="modal-header text-start">
+                                            <h4 class="modal-title w-100 font-weight-bold">Tambah Akun User</h4>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"
+                                                style="color: white;">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+
+                                        </div>
+                                        <form class="" method="POST" action="">
+                                            @csrf
+                                            <div class="modal-body mx-3">
+                                                <div class="mb-5">
+                                                    <i class="iconify nav-icon mr-3" data-icon="ic:outline-email"></i>
+                                                    <label data-error="wrong" data-success="right"
+                                                        for="defaultForm-email">Email</label>
+                                                    <input type="email" name="email" id="defaultForm-email"
+                                                        class="form-control validate" placeholder="Input email">
+                                                </div>
+
+                                                <div class="mb-5">
+                                                    <i class="mr-3 fa-regular fa-user"></i>
+                                                    <label data-error="wrong" data-success="right"
+                                                        for="defaultForm-Username">Username</label>
+                                                    <input type="text" name="name" id="defaultForm-username"
+                                                        class="form-control validate" placeholder="Input nama">
+                                                </div>
+
+                                                <div class="mb-5">
+                                                    <i class="iconify nav-icon mr-3"
+                                                        data-icon="teenyicons:password-outline"></i>
+                                                    <label data-error="wrong" data-success="right"
+                                                        for="defaultForm-pass">Password</label>
+                                                    <input type="password" name="password" id="defaultForm-pass"
+                                                        class="form-control validate" placeholder="Input password">
+                                                </div>
+
+                                                <div class="mb-5">
+                                                    <div class="d-flex align-items-center">
+                                                        <i class="iconify" data-icon="carbon:user-role"></i>
+                                                        <select class="form-select border-0 w-50 ml-3"
+                                                            aria-label="Default select example">
+                                                            <option selected value="teknisi">Teknisi</option>
+                                                            <option selected value="klien">Klien</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+
+
+                                            </div>
+                                            <div class="modal-footer d-flex justify-content-center">
+                                                <button type="submit" class="btn btn-success">Buat Akun</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- /modal -->
+
+                            <!-- Button Modal -->
+                            <div class="text-center">
+                                <a href="" class="btn btn-light btn-rounded mb-4" data-toggle="modal"
+                                    style="color:#12ACED" data-target="#modalLoginForm"><i class="iconify nav-icon mr-3"
+                                        data-icon="line-md:account-add"></i>Tambah Akun</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <!-- Main content -->
-                <section class="content">
-                    <div class="container-fluid">
+                {{-- <section class="content">
+                    <div class="container-fluid p-5">
                         <div class="card-tools">
                             <h6 style="color: white;">Cari Akun Pengguna:</h6>
                             <div class="input-group input-group-sm" style="width: 150px;">
@@ -24,16 +112,16 @@
                                     </button>
                                 </div>
                                 <div class="col-auto">
-                                    <a href="{{ route('tambahAkun') }}" class="btn btn-light mb-3 float-right">Tambah Data</a>
+                                    <a href="{{ route('tambahAkun') }}" class="btn btn-light mb-3 float-right">Tambah
+                                        Data</a>
                                 </div>
 
                             </div>
                         </div>
-                        {{-- <div class="col-12">
-                    <a href="{{ route('create') }}" class="btn btn-primary mb-3">Tambah Data Produk</a> --}}
 
                     </div>
-            </div> <!-- /.card -->
+                </section> <!-- /.card --> --}}
+            </div>
         </div>
         <div class="card">
             <div class="card-header">
@@ -55,8 +143,8 @@
                         @foreach ($data as $d)
                             <tr>
                                 <td>{{ $d->name }}</td>
-                                <td>{{ $d->password}}</td>
-                                <td>{{ $d->role}}</td>
+                                <td>{{ $d->password }}</td>
+                                <td>{{ $d->role }}</td>
                                 <td>{{ $d->status }}</td>
                                 <td>
 
@@ -73,7 +161,8 @@
                                     <div class="modal-content">
                                         <div class="modal-header">
                                             <h4 class="modal-title">Default Modal</h4>
-                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <button type="button" class="close" data-dismiss="modal"
+                                                aria-label="Close">
                                                 <span aria-hidden="true">&times;</span>
                                             </button>
                                         </div>
